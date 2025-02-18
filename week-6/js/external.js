8e757b3a703f166905dbd3159f1466f4925ee7cd
ayaan23 <aa52@iu.edu>
2025-02-18 11:34:23 -0500

    week 6 js

---
+++ week-6/js/external.js
@@ -0,0 +1,18 @@
+console.log("Ummmmm....Where ARE We???");
+
+const birthYear = 1207;
+
+let myName = "Ayaan the Noodle Farm Barn";
+
+console.log(myName, "was born in", birthYear);
+
+// constant variables can't change their value
+// birthYear = 1208;  
+
+console.warn(myName, "MAY have devoured 10 souls");
+
+console.log(myName, "is", myName.length, "characters long.");
+
+console.log("myName includes 'ty':", myName.toUpperCase().includes("TY"));
+
+console.log("myName has uppercase:", myName !== myName.toLowerCase());
