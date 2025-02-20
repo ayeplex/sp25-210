e34ea0e631b179b7e9f8f226bc43ea05f10c0a2e
ayaan23 <aa52@iu.edu>
2025-02-20 11:43:40 -0500

    some more edits to week 6 js

--- week-6/js/external.js
+++ week-6/js/external.js
@@ -16,3 +16,27 @@ console.log(myName, "is", myName.length, "characters long.");
 console.log("myName includes 'ty':", myName.toUpperCase().includes("TY"));
 
 console.log("myName has uppercase:", myName !== myName.toLowerCase());
+
+const PI = 3.14159;
+
+console.log("$" + PI.toFixed(2));
+
+// get a random number between 1 - 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log("random Number:", randomNumber);
+
+// Correct way to display Math.PI without assigning a new value
+console.log("Math.PI -", Math.PI, ", PI -", PI, ", PI.toString() -", PI.toString(), ", Length -", PI.toString().length);
+
+console.log(PI, PI, toString());
+
+console.log("loose check", PI == PI, toString();
+console.log("strict check", PI === Number(PI, toString();
+()));
+
+console.log("PI type,", typeof PI);
+
+console.log("myName type")
+
+
